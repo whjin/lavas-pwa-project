ad72a8e9ddc8c0f3e7a449e3507cfc5c8c9b7755
whjin <1414848321@qq.com>
2018-09-14 12:31:30 +0800

    lavas-pwa-project

---
+++ dist/precache-manifest.41c905134e72b7e21384ebf6e42a2b72.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3742528143340e36915adf388d64060f",
+    "url": "/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "3dcec28f64bfae8c2232",
+    "url": "/static/js/vendor.3dcec28f.js"
+  },
+  {
+    "revision": "2c5d37a108202e131e2d841af6e51c63",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "c8c38165216e7ab28275",
+    "url": "/static/js/vue.c8c38165.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/js/workbox-sw.prod.v2.1.2.js"
+  },
+  {
+    "revision": "047f3021e153c19951f86dd209904243",
+    "url": "/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b8a42c4a3b00c4f75bc636f2923eaeca",
+    "url": "/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "83409fb3fe88cb94d7d3df5fe97569c9",
+    "url": "/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "588079df8f1b8fde3b3cd691ee65bf06",
+    "url": "/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "453bad73fa1e0ff73ed2",
+    "url": "/static/js/index.453bad73.js"
+  },
+  {
+    "revision": "4dc6fdc67611efce36a3c500cbe1c0ec",
+    "url": "/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "b451e38c6ee4b03781b1019c518e7894",
+    "url": "/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "7253881ecb893ceb68f6",
+    "url": "/static/js/manifest.7253881e.js"
+  },
+  {
+    "revision": "0ff69c8fca81469c28f589d5fad0616e",
+    "url": "/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "efbe0aa485b3aeaceb8ead9191f0c4c0",
+    "url": "/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "453bad73fa1e0ff73ed2",
+    "url": "/static/css/index.9ba72274.css"
+  },
+  {
+    "revision": "6bb6ca99e9d109454b9825b56a89f8d8",
+    "url": "/appshell"
+  }
+];
